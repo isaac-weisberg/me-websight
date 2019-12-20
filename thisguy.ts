676c8c5fbc1733f0e08cdc3dad863c699fb0e143
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-12-20 21:53:13 +0300

    Update the colors lib

--- thisguy.ts
+++ thisguy.ts
@@ -12,19 +12,19 @@ function apply(styling: Styling, element: HTMLElement) {
 
 const themes: Styling[] = [
     {
-        backgroundColor: "#9B1B30",
-        textColor: "#FFFFFF"
+        backgroundColor: "#F5D6C6",
+        textColor: "#000000"
     },
     {
         backgroundColor: "#2A4B7C",
         textColor: "#FFFFFF"
     },
     {
-        backgroundColor: "#F5D6C6",
-        textColor: "#000000"
+        backgroundColor: "#CE5B78",
+        textColor: "#FFFFFF"
     },
     {
-        backgroundColor: "#CE5B78",
+        backgroundColor: "#577284",
         textColor: "#FFFFFF"
     },
     {
@@ -42,10 +42,21 @@ function nextIndex(): number {
     return index
 }
 
-function nextStyling() {
-    const randomIndex = nextIndex()
-    const styling = themes[randomIndex]
+function initialStyling() {
+    const styling = themes[index]
+    applyTheme(styling)
+}
+
+function applyTheme(styling: Styling) {
     colorableElements.forEach(element => {
         apply(styling, element)
     })
 }
+
+function nextStyling() {
+    const randomIndex = nextIndex()
+    const styling = themes[randomIndex]
+    applyTheme(styling)
+}
+
+initialStyling()
